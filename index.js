fb1685e0ad58062a44a3254b475695b46f03221b
amarisng <amarissynergylimited@gmail.com>
2026-08-15 10:48:58 -0700

    Add route for ambassador-kit page

--- index.js
+++ index.js
@@ -721,6 +721,9 @@ app.get('/content-writing', (req, res) => {
 app.get('/ambassador', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'ambassador.html'));
 });
+app.get('/ambassador-kit', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'ambassador-kit.html'));
+});
 app.get('/digital-marketing', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'digital-marketing.html'));
 });
